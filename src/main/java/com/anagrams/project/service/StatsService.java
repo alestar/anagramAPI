9f6369fd3092534e79d1c8dcd8f1304f802e105d
alestar <alejandrocoutin@esnoei.com>
2019-05-12 21:25:37 -0600

    - Renamed

--- src/main/java/com/anagrams/project/service/StatsService.java
+++ src/main/java/com/anagrams/project/service/StatsService.java
@@ -1,11 +1,9 @@
 package com.anagrams.project.service;
 
-import com.anagrams.project.model.StatsModel;
+import com.anagrams.project.model.Stats;
 import com.anagrams.project.repository.AnagramRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.*;
 
 @Component
 public class StatsService {
@@ -13,16 +11,16 @@ public class StatsService {
     @Autowired
     AnagramRepository anagramRepository;
 
-    public StatsModel calculateStats (){
-        StatsModel statsModel = new StatsModel();
+    public Stats calculateStats (){
+        Stats stats = new Stats();
 
         Long totalRows =anagramRepository.count();
 
-        statsModel.setAverageWordLength(anagramRepository.getLengthAverage());
-        statsModel.setMaxWordsLength(anagramRepository.getMaxLength());
-        statsModel.setMinWordsLength(anagramRepository.getMinLength());
-        statsModel.setMedianWordsLength(anagramRepository
+        stats.setAverageWordLength(anagramRepository.getLengthAverage());
+        stats.setMaxWordsLength(anagramRepository.getMaxLength());
+        stats.setMinWordsLength(anagramRepository.getMinLength());
+        stats.setMedianWordsLength(anagramRepository
                 .findById(totalRows/2).get().getLength());
-     return statsModel;
+     return stats;
     }
 }
