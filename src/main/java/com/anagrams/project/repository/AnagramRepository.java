948ed61e834abef8bddea07c3ba437743efb87c0
alestar <alejandrocoutin@esnoei.com>
2019-05-13 02:41:43 -0600

    - Trying to fix SQL queries for for >= groupSize - Trying to fix SQL queries for finding the median of lengths

--- src/main/java/com/anagrams/project/repository/AnagramRepository.java
+++ src/main/java/com/anagrams/project/repository/AnagramRepository.java
@@ -4,18 +4,23 @@ package com.anagrams.project.repository;
 import com.anagrams.project.entity.Anagram;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AnagramRepository extends JpaRepository<Anagram, Long> {
 
+    Optional<Anagram> findById(Long id);
+
     Anagram findByToken(String token);
 
     List<Anagram> findAllByLength(Integer length);
+
+    //@Query(value = "SELECT FROM Anagram a WHERE a.length >= size  ")// To fix
+    //List<Anagram>  getGroupSize(int size);
 
     @Query("Select AVG(a.length) from Anagram a")
     int getLengthAverage();
@@ -28,6 +33,9 @@ public interface AnagramRepository extends JpaRepository<Anagram, Long> {
 
     @Query(value = "SELECT TOP 1  WORDS FROM ANAGRAM GROUP BY WORDS HAVING MAX(VOLUME) order by  volume desc", nativeQuery = true)
     String getMaxVolume();
+
+    //@Query( "SELECT a.length FROM Anagram GROUP BY LENGHT HAVING a ORDER BY length desc ")// To fix
+    //int getMedianLength();
 
     @Transactional
     Long removeAnagramByToken(String token);
